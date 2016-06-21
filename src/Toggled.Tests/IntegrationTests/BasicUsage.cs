57274b1b082591b97d3b0d5a1fa74894c81ce2e7
John Du Hart V <jduhart@hhogdev.com>
2016-06-21 13:56:23 -0400

    Adding integration test to demonstrate basic usage

---
+++ src/Toggled.Tests/IntegrationTests/BasicUsage.cs
@@ -0,0 +1,37 @@
+﻿using Toggled.Tests.Helpers;
+using Toggled.Toggles;
+using Toggled.Traits;
+using Xunit;
+
+namespace Toggled.Tests.IntegrationTests
+{
+    public class BasicUsage
+    {
+        private readonly IFeature _testFeature = FeatureBuilder.Create("MyTestingFeature")
+            .Description("This is my basic test feature")
+            .WithDefaultValue(true)
+            .Build();
+
+        private readonly IFeatureContext _featureContext;
+
+        public BasicUsage()
+        {
+            _featureContext = new FeatureContext(new FeatureToggleProvider(new DefaultValueToggle()));
+        }
+
+        [Fact]
+        public void FeatureContextHasDefaultValue()
+        {
+            Assert.True(_featureContext.IsEnabled(_testFeature));
+        }
+
+        [Fact]
+        public void FeatureHasDefaultValue()
+        {
+            using (ContextSwitcher.For(_featureContext))
+            {
+                Assert.True(Feature.IsEnabled(_testFeature));
+            }
+        }
+    }
+}
